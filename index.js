6ac9a4ccce6f2a4247cc8e4fce4368bd7874b471
anuj agarwal <anujagrawal@outlook.in>
2020-06-14 02:22:20 +0530

    set view engine in index js file

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app=express();
 const port=8000;
 // use express router
 app.use('/',require('./routes'))
+app.set('view engine' , 'ejs');
+//app.set('views',path.join(__dirname,'views'))
+app.set('views','./views')
+
 app.listen(port,function(err){
     if(err){
         //console.log('Error: ',err);
